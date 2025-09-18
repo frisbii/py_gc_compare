ac52b60a5dd677bd3f5d91a604895ecb8532f113
Madi Gudin <mgudin27@amherst.edu>
2025-09-18 00:18:21 -0400

    relative speedup output

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 import time
 import sys
 
+time_base = 0
+
 def baseline():
     x = 0
     for i in range(100000):
         x += i
     return x
 
-def main():
+def mac():
+    x = 0
+    for i in range(100000):
+        x += i*i
+    return x
+
+def short_lives():
+    for i in range(100000):
+        x = [0,1,2,3]
+    return 0
+
+def short_cycles():
+    for i in range(100000):
+        a, b = {}, {}
+        a["fiz"] = b
+        b["baz"] = a
+    return 0
+
+def run(benchmark):
+    global time_base
     times = []
     for i in range(100):
         t0 = time.time()
-        baseline()
+        benchmark()
         t1 = time.time()
         times.append(t1 - t0)
+    bench_name = benchmark.__name__
     avg = sum(times) / len(times)
-    print("avg: %.10f" % avg)
 
-main()
+    if bench_name == "baseline":
+        print("\t" + ("%s avg: " % bench_name).ljust(20) + "%.10f" % avg)
+        time_base = avg
+    else:
+        speedup = time_base / avg
+        print("\t" + ("%s avg: " % bench_name).ljust(20) + "%.10f" % avg + "\t\tspeedup: %.3fx" % speedup)
+
+
+
+if __name__ == "__main__":
+    tests = [baseline, mac, short_lives, short_cycles]
+    for test in tests:
+        run(test)
